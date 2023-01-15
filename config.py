72cc5bf7819ca459c570834fec1b068763eee7b7
unknown <you@example.com>
2023-01-15 19:41:26 +0530

    project File Path

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+import project_data
+
+PORT_NUMBER =5000
+MODEL_FILE_PATH = os.path.join("project_data","Adaboost_model.pkl")
+SCALING_FILE_PATH = os.path.join("project_data","Scaling.pkl")
+JSON_FILE_PATH = os.path.join("project_data","project_data.json")
